413650d518baadea26016ddc782f3b7a85afc2e3
Rick Foos <rfoos@solengtech.com>
2017-12-19 17:30:45 -0600

    Add openocd packages for Ubuntu 16.04.3

---
+++ ubuntu/ocd-autoconf.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Rick Foos <rfoos at solengtech.com>
+
+# Packages to do an autoconf build of OpenOCD on fresh LTS Ubuntu.
+
+prompt_install="Do you want to install these packages now (y|n)?"
+prompt_please="\nPlease answer y, or n"
+
+cat << EOM
+Root or Sudo priviledges are required to do this. If you do not have
+sufficient priviledge, copy the install command from above, and have
+your admin install the packages.
+
+EOM
+
+while true; do
+read -r -p  "${prompt_install}" yn
+case $yn in
+    [Yy]* ) echo "Starting package install (sudo required)."; break;;
+    [Nn]* ) printf "\nInstall canceled.\n"; exit 0;;
+    * )     printf "${prompt_please}";;
+esac
+done
+
+sudo apt-get -y update && sudo apt-get -y upgrade && \
+sudo apt-get -y install openssh-server build-essential gcc g++ git make \
+    m4 python tar zip unzip bzip2 libtool pkg-config automake \
+    autoconf autotools-dev gettext texinfo texlive flex doxygen graphviz \
+    dos2unix libusb-1.0-0-dev libusb-1.0-0 libftdi1 libftdi1-2 \
+    libhidapi-dev libhidapi-hidraw0 libhidapi-libusb0
+    
